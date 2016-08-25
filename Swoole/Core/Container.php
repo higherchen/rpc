c1f570ecfe05ebd46a2e2b190bcde64424c61b5f
higher <chenzhenhai@bilibili.com>
2016-08-25 20:18:05 +0800

    add Di module

---
+++ Swoole/Core/Container.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Swoole\Core;
+
+class Container
+{
+    protected $_services = [];
+    protected $_instances = [];
+
+    public function get($alias)
+    {
+        return isset($this->_services[$alias]) ? $this->_services[$alias] : null;
+    }
+
+    public function set($alias, $instance)
+    {
+        if (!isset($this->_services[$alias])) {
+            $this->_services[$alias] = $instance;
+        }
+    }
+
+    public function singleton($alias, $parameters = [])
+    {
+        if (isset($this->instances[$alias])) {
+            return $this->instances[$alias];
+        }
+
+        if (isset($_services[$alias]) && is_callable($_services[$alias])) {
+            $object = call_user_func_array($_services[$alias], $parameters);
+        } else {
+            $class = new \ReflectionClass($alias);
+            $object = $class->newInstanceArgs($parameters);
+        }
+
+        if ($object !== null) {
+            $this->instances[$abstract] = $object;
+        }
+
+        return $object;
+    }
+
+    public function make($alias, $parameters = [], $shared = false)
+    {
+        if ($shared && isset($this->instances[$alias])) {
+            return $this->instances[$alias];
+        }
+
+        if (isset($_services[$alias]) && is_callable($_services[$alias])) {
+            $object = call_user_func_array($_services[$alias], $parameters);
+        } else {
+            $class = new \ReflectionClass($alias);
+            $object = $class->newInstanceArgs($parameters);
+        }
+
+        if ($shared && $object !== null) {
+            $this->instances[$abstract] = $object;
+        }
+
+        return $object;
+    }
+}
---
+++ Swoole/Core/Di.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Swoole\Core;
+
+class Di
+{
+    protected static $_instance;
+
+    public static function __callStatic($method, $args)
+    {
+        if (!static::$_instance) {
+            static::$_instance = new Container();
+        }
+        $instance = static::$_instance;
+
+        switch (count($args)) {
+            case 0:
+                return $instance->$method();
+            case 1:
+                return $instance->$method($args[0]);
+            case 2:
+                return $instance->$method($args[0], $args[1]);
+            case 3:
+                return $instance->$method($args[0], $args[1], $args[2]);
+            case 4:
+                return $instance->$method($args[0], $args[1], $args[2], $args[3]);
+            default:
+                return call_user_func_array([$instance, $method], $args);
+        }
+    }
+}
