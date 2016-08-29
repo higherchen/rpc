9dbad8b201fca98dde0f23c86ae46730761b4267
higher-chen <higher-chen@higher-chendeMacBook-Pro.local>
2016-08-29 12:31:38 +0800

    add resolve

--- Swoole/MySQL/Resolve.php
+++ Swoole/MySQL/Resolve.php
@@ -4,13 +4,50 @@ namespace Swoole\MySQL;
 
 class Resolve
 {
+    protected $conn;
+    protected $data;
+    protected $prepared = false;
+    protected $stmt = null;
+
+    protected static $stmts = [];
+
     public function __construct($conn, $data) 
     {
-        
+        $this->conn = $conn;
+        $this->data = $data;
+    }
+
+    public function getStatement($sql, $options = null)
+    {
+        $mark = md5($sql);
+        if (!isset(static::$stmts[$mark])) {
+            static::$stmts[$mark] = $this->conn->prepare($sql, $options);
+        }
+
+        return static::$stmts[$mark];
     }
 
     public function run()
     {
-        
+        $result = '';
+        foreach ($this->data as $command) {
+            foreach ($command as $method => $params) {
+                if ($method == 'prepare') {
+                    $this->stmt = $this->getStatement($params[0], $params[1]);
+                    $this->prepared = true;
+                    continue;
+                }
+                if ($this->prepared && method_exists($this->stmt, $method)) {
+                    $result = call_user_func_array([$this->stmt, $method], $params);
+                    continue;
+                }
+                if (!$this->prepared && method_exists($this->conn, $method)) {
+                    $result = call_user_func_array([$this->conn, $method], $params);
+                    continue;
+                }
+            }
+        }
+
+        return $result;
     }
 }
