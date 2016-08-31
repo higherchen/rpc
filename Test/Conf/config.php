01d3f8a170730c3061fea4f146130d887d40229f
higher-chen <higher-chen@higher-chendeMacBook-Pro.local>
2016-08-31 12:03:04 +0800

    fix bug

--- Test/Conf/config.php
+++ Test/Conf/config.php
@@ -3,4 +3,6 @@
 return [
     'host' => '0.0.0.0',
     'port' => 8091,
+    'processor' => '\\Services\\HelloSwoole\\HelloSwooleProcessor',
+    'handler' => '\\Services\\HelloSwoole\\Handler',
 ];
